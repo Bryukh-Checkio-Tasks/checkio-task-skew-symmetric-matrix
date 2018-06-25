6ed61b95eb3f13060ae0f3e3064fc59dffe667af
XanderVi <engineermexanic@gmail.com>
2018-06-25 15:21:03 +0300

    adapt for JS

--- verification/referee.py
+++ verification/referee.py
@@ -1,9 +1,20 @@
 from checkio.signals import ON_CONNECT
 from checkio import api
 from checkio.referees.io import CheckiOReferee
+from checkio.referees import cover_codes
 
 from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(
+        tests=TESTS,
+        function_name={
+            "python": "checkio",
+            "js": "symmetric"
+        },
+        cover_code={
+            #'python-3': cover_codes.unwrap_args,
+            #'js-node': cover_codes.js_unwrap_args
+        }
+    ).on_ready)
